24c076c0fc6fe35c69dcd2ed711fc8b19a9e64aa
Tetiana Brovchenko <tmbrovchenko@gmail.com>
2021-02-11 15:51:26 +0100

    Implement Main Task. Task 2.

---
+++ src/main/java/pl/epam/course/ta/level1/fundamentals/main/CommandLineArguments.java
@@ -0,0 +1,18 @@
+package pl.epam.course.ta.level1.fundamentals.main;
+
+import java.util.Scanner;
+
+public class CommandLineArguments {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter numbers : ");
+        int value = scanner.nextInt();
+        int reversdigits = 0;
+        while (value != 0) {
+            reversdigits = reversdigits * 10 + (value % 10);
+            value = value / 10;
+        }
+        System.out.print("Output of the reverse sequence of digits of a number: " + reversdigits);
+
+    }
+}
