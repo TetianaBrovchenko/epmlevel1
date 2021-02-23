af9c54d6a2e1fca686bfd675be3b15dfad2bc176
Tetiana Brovchenko <tmbrovchenko@gmail.com>
2021-02-23 17:02:45 +0100

    variable introduction

--- src/main/java/pl/epam/course/ta/level1/fundamentals/optional1/ArrayAssistantFromConsole.java
+++ src/main/java/pl/epam/course/ta/level1/fundamentals/optional1/ArrayAssistantFromConsole.java
@@ -1,20 +1,20 @@
 package pl.epam.course.ta.level1.fundamentals.optional1;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class ArrayAssistantFromConsole {
+
     //Задание. Ввести n чисел с консоли.
     public static int[] createArrayOfNumbers() {
         Scanner in = new Scanner(System.in);
         System.out.println("введите размер массива");
-        int l = in.nextInt();
-        int[] arrayOfNumbers = new int[l];
+        int arraySize = in.nextInt();
+        int[] arrayOfNumbers = new int[arraySize];
         System.out.println("введите масив");
-        for (int i = 0; i < arrayOfNumbers.length; i++) {
+        for (int i = 0; i < arraySize; i++) {
             arrayOfNumbers[i] = in.nextInt();
         }
-        for (int i = 0; i < arrayOfNumbers.length; i++) {
+        for (int i = 0; i < arraySize; i++) {
             System.out.print(arrayOfNumbers[i] + "  ");
         }
         System.out.println();
