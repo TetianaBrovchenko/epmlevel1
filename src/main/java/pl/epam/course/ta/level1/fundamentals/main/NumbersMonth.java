8a3ce096dfab1433dee9df375c1e69aa862ff40e
Tetiana Brovchenko <tmbrovchenko@gmail.com>
2021-02-23 15:16:29 +0100

    method name changes

--- src/main/java/pl/epam/course/ta/level1/fundamentals/main/NumbersMonth.java
+++ src/main/java/pl/epam/course/ta/level1/fundamentals/main/NumbersMonth.java
@@ -7,14 +7,14 @@ public class NumbersMonth {
         //5. Ввести число от 1 до 12. Вывести на консоль название месяца, соответствующего данному числу. Осуществить проверку корректности ввода чисел.
         System.out.print("Please enter the number of month: ");
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        System.out.println(solve(n));
+        int monthNumber  = scan.nextInt();
+        System.out.println(findMonthByNumber(monthNumber));
     }
 
-    public static String solve(int n) {
+    public static String findMonthByNumber(int monthNumber) {
         String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        if (n > 0 && n <= month.length) {
-            return month[n-1];
+        if (monthNumber > 0 && monthNumber <= month.length) {
+            return month[monthNumber-1];
         }
         return "Month not found";
     }
